c9e649751e306045bca4ab8fcfa7992ac46c3783
lior <liorazar456@gmail.com>
2025-07-15 10:52:49 +0300

    second question

--- main.py
+++ main.py
@@ -5,15 +5,19 @@ def print_movies(movies: list[str]):
 
 
 def main():
-    movies = []
+    movies = {}
 
     while True:
         movie = input("enter movie name, exit to quit: ")
         if movie.strip().lower() == "exit":
             break
-        movies.append(movie)
+        if movie in movies.keys():
+            print("movie already exist")
+            continue
+        ranking = input("enter movie ranking, from 1 to 10: ")
+        movies[movie] = ranking
 
-    print_movies(movies)
+    print_movies(movies.keys())
 
 
 if __name__ == '__main__':
