2bbcbb3143e3adec4fa5fc2cf692a31c99a76d8e
lior <liorazar456@gmail.com>
2025-07-15 11:02:45 +0300

    third question

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ def print_movies(movies: list[str]):
         print(f"{i}. {m}")
 
 
+def print_ranking_stats(movies: dict[str, int]):
+    rankings = movies.values()
+    print(f"\033[31maverage ranking is {sum(rankings) / len(rankings)}\033[0m")
+    movie_with_max_value = max(movies, key=movies.get)
+    print(f"\033[38;5;208mmovie with max ranking is: {movie_with_max_value}\033[0m")
+    movie_with_min_value = min(movies, key=movies.get)
+    print(f"\033[95mmovie with min ranking is: {movie_with_min_value}\033[0m")
+
+
 def main():
     movies = {}
 
@@ -15,9 +24,9 @@ def main():
             print("movie already exist")
             continue
         ranking = input("enter movie ranking, from 1 to 10: ")
-        movies[movie] = ranking
+        movies[movie] = int(ranking)
 
-    print_movies(movies.keys())
+    print_ranking_stats(movies)
 
 
 if __name__ == '__main__':
